7bc4b7f87a21d6be547716b379a076b513963f04
Evan Krambuhl <thekrambuhl@gmail.com>
2016-06-23 20:12:26 -0700

    jshint  and deps

--- test/index.test.js
+++ test/index.test.js
@@ -8,7 +8,7 @@ const Component = {
 };
 
 test('<Each data={undefined}><div /></Each>', function(t) {
-  const tree = Object.assign({ }, Component, { 
+  const tree = Object.assign({ }, Component, {
     attribs: { data: undefined }
   });
 
@@ -19,10 +19,9 @@ test('<Each data={undefined}><div /></Each>', function(t) {
 });
 
 test('<Each data={emptyArray}><div /></Each>', function(t) {
-  const tree = Object.assign({ }, Component, { 
+  const tree = Object.assign({ }, Component, {
     attribs: { data: [] }
   });
-
   const res = Each(tree);
 
   t.plan(1);
@@ -30,10 +29,9 @@ test('<Each data={emptyArray}><div /></Each>', function(t) {
 });
 
 test('<Each data={emptyObject}><div /></Each>', function(t) {
-  const tree = Object.assign({ }, Component, { 
+  const tree = Object.assign({ }, Component, {
     attribs: { data: { } }
   });
-
   const res = Each(tree);
 
   t.plan(1);
@@ -41,10 +39,9 @@ test('<Each data={emptyObject}><div /></Each>', function(t) {
 });
 
 test('<Each data={nonEmptyArray}><div /></Each>', function(t) {
-  const tree = Object.assign({ }, Component, { 
+  const tree = Object.assign({ }, Component, {
     attribs: { data: ['a', 'b', 'c'] }
   });
-
   const res = Each(tree);
 
   t.plan(11);
@@ -62,10 +59,9 @@ test('<Each data={nonEmptyArray}><div /></Each>', function(t) {
 });
 
 test('<Each data={nonEmptyObject}><div /></Each>', function(t) {
-  const tree = Object.assign({ }, Component, { 
+  const tree = Object.assign({ }, Component, {
     attribs: { data: { a: 'aaa', b: 'bbb', c: 'ccc' } }
   });
-
   const res = Each(tree);
 
   t.plan(13);
@@ -87,22 +83,21 @@ test('<Each data={nonEmptyObject}><div /></Each>', function(t) {
 
 const arrayMap = require('../dist/arrayMap');
 test('tree.arrayMap(a, 0) :: with attribs.as', function(t) {
-  const tree = Object.assign({ }, Component, { 
-    attribs: { as: 'letter', data: ['a', 'b', 'c'] }
+  const tree = Object.assign({ }, Component, {
+    attribs: { as: '@letter', data: ['a', 'b', 'c'] }
   });
-
   const res = arrayMap.call(tree, 'b', 1);
 
   t.plan(4);
   t.equal(res.type, 'frame');
   t.equal(res.locals['@index'], 1);
   t.equal(res.locals['@length'], 3);
-  t.equal(res.locals['letter'], 'b');
+  t.equal(res.locals['@letter'], 'b');
 });
 
 const objectMap = require('../dist/objectMap');
 test('tree.objectMap(key:b, 2)', function(t) {
-  const tree = Object.assign({ }, Component, { 
+  const tree = Object.assign({ }, Component, {
     attribs: { data: { a: 'aaa', b: 'bbb', c: 'ccc' } }
   });
 
